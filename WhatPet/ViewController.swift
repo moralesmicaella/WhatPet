4e27bee7300374f69f09d9bbb40c839fe4c92a26
Micaella Morales <morales.micaella@gmail.com>
2022-01-06 00:58:23 -0800

    Add UI elements

--- WhatPet/ViewController.swift
+++ WhatPet/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var petImageView: UIImageView!
+    @IBOutlet var labelView: UIView!
+    @IBOutlet var petLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        print("camera tapped")
+    }
+    
 }
 
